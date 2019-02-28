8fb18446d15ab1697bc28b09719f1b913e822797
codecolab69 <wanderlust9x@gmail.com>
2019-02-28 12:40:25 +0530

    atom commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1> whats up bitches !! holaa from atom ..!!!</h1>
+      <h1> whats up bitches !! holaa from atom ..!!</h1>
+
 
     );
   }
